c0f2f730bf995b58920ecb8a60ba22411821e030
DeringSalazar <desalazarsa@est.utn.ac.cr>
2025-11-14 17:12:53 -0600

    Configuracion basica
    Se realiza la configuracion para que la parte visual este conectada al mainActivity

--- app/src/main/java/com/example/hotelsync/MainActivity.java
+++ app/src/main/java/com/example/hotelsync/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.hotelsync;
 
 import android.os.Bundle;
+import android.widget.*;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText TxtCedula, TxtContra;
+    RadioButton BtnHuesped, BtnEmpleado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        TxtCedula = findViewById(R.id.TxtCedula);
+        TxtContra = findViewById(R.id.TxtContra);
+        BtnHuesped = findViewById(R.id.BtnHuesped);
+        BtnEmpleado = findViewById(R.id.BtnEmpleado);
     }
+
+
 }
